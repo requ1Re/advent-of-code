b89d4e8d32dc9074771d58a7f00e7e4731aa0d26
Patrick List <patrick.list@pm.me>
2024-12-05 14:38:14 +0100

    Day 1

---
+++ 2024/1/solution.ts
@@ -0,0 +1,30 @@
+// https://adventofcode.com/2024/day/1
+
+const input = await Deno.readTextFile(`${Deno.cwd()}/2024/1/input.txt`);
+
+const lines = input.split('\n');
+const [list1, list2] = [0, 1].map(i => lines.map(line => +line.split('   ')[i]).sort((a, b) => +a - +b));
+
+// Part 1
+let totalDistance = 0;
+
+for (let i = 0; i < list1.length; i++) {
+    const dist = Math.abs(list1[i] - list2[i]);
+    totalDistance += dist;
+}
+
+console.log('Part 1:', totalDistance);
+
+
+
+// Part 2
+let totalSimilarityScore = 0;
+
+for(const numList1 of list1){
+    const occurrancesInSecondList = list2.filter(numList2 => numList2 === numList1).length;
+    const similarityScore = numList1 * occurrancesInSecondList;
+
+    totalSimilarityScore += similarityScore;
+}
+
+console.log('Part 2:', totalSimilarityScore);
