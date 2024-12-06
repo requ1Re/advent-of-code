49207eaa7a5cc64477e6997caf3d1be898f947d4
Patrick List <patrick.list@pm.me>
2024-12-07 00:33:06 +0100

    Day 4 (Part 2)

--- 2024/4/solution.ts
+++ 2024/4/solution.ts
@@ -2,7 +2,7 @@
 
 import { getInput, getLineSeparator, transpose } from "../../util.ts";
 
-const input = await getInput(import.meta);
+const input = await getInput(import.meta, false);
 
 const grid = input.split(getLineSeparator()).map(line => line.split(''));
 
@@ -14,50 +14,55 @@ let totalXmases = 0;
 for (const line of input.split(getLineSeparator())) {
     totalXmases += line.matchAll(/XMAS/g).toArray().length + line.matchAll(/SAMX/g).toArray().length;
 }
-console.log('Part 1 Horizontal:', totalXmases);
 
 // Vertical
-let _totalXmasesPrev = totalXmases;
 const rotatedGrid = transpose(grid);
 for (const _line of rotatedGrid) {
     const line = _line.join('');
-    console.log(line);
     totalXmases += line.matchAll(/XMAS/g).toArray().length + line.matchAll(/SAMX/g).toArray().length;
 }
 
-console.log('Part 1 Vertical:', totalXmases - _totalXmasesPrev);
-
 // Diagonal
-_totalXmasesPrev = totalXmases;
 for (let y = 0; y < grid.length; y++) {
     const line = grid[y];
 
     for (let x = 0; x < line.length; x++) {
         const strForwards = getDiagonal(grid, y, x);
-        if(strForwards === "XMAS" || strForwards === 'SAMX') totalXmases++;
+        if (strForwards === "XMAS" || strForwards === 'SAMX') totalXmases++;
 
         const strBackwards = getDiagonal(grid, y, x, false);
-        if(strBackwards === "XMAS" || strBackwards === 'SAMX') totalXmases++;
+        if (strBackwards === "XMAS" || strBackwards === 'SAMX') totalXmases++;
     }
 }
-console.log('Part 1 Diagonal:', totalXmases - _totalXmasesPrev);
 
-console.log('Part 1 Total:', totalXmases);
+console.log('Part 1:', totalXmases);
 
-function getDiagonal(grid: string[][], y: number, x: number, forwards = true): string {
-    if(y+3 >= grid.length) return "";
-    if(forwards && x+3 > grid[0].length) return "";
-    if(!forwards && x-3 < 0) return "";
+// Part 2
+totalXmases = 0;
+for (let y = 0; y < grid.length; y++) {
+    const line = grid[y];
+
+    const matches = line.join('').matchAll(/(M|S)/g);
+
+    for(const match of matches){
+        const diag1 = getDiagonal(grid, y, match.index, true, 2);
+        const diag2 = getDiagonal(grid, y, match.index+2, false, 2);
+
+        if((diag1 === 'MAS' || diag1 === 'SAM') && (diag2 === 'MAS' || diag2 === 'SAM')) totalXmases++;
+    }
+}
+console.log('Part 2:', totalXmases);
+
+function getDiagonal(grid: string[][], y: number, x: number, forwards = true, steps = 3): string {
+    if (y + steps >= grid.length) return "";
+    if (forwards && x + steps > grid[0].length) return "";
+    if (!forwards && x - steps < 0) return "";
 
     return (forwards ? [
         grid[y][x],
-        grid[y + 1][x + 1],
-        grid[y + 2][x + 2],
-        grid[y + 3][x + 3]
+        ...Array.from(Array(steps).keys()).map(i => i + 1).map((i) => grid[y + i][x + i])
     ] : [
         grid[y][x],
-        grid[y + 1][x - 1],
-        grid[y + 2][x - 2],
-        grid[y + 3][x - 3]
+        ...Array.from(Array(steps).keys()).map(i => i + 1).map((i) => grid[y + i][x - i])
     ]).join('');
 }
