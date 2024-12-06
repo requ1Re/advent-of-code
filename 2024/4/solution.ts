cf4a9f064bf0a7420fbda04b1a6e7ae037ef708e
Patrick List <patrick.list@pm.me>
2024-12-07 00:03:33 +0100

    Day 4 (Part 1)

--- 2024/4/solution.ts
+++ 2024/4/solution.ts
@@ -1,8 +1,8 @@
 // https://adventofcode.com/2024/day/4
 
-import { getInput, getLineSeparator } from "../../util.ts";
+import { getInput, getLineSeparator, transpose } from "../../util.ts";
 
-const input = await getInput(import.meta, true);
+const input = await getInput(import.meta);
 
 const grid = input.split(getLineSeparator()).map(line => line.split(''));
 
@@ -16,69 +16,48 @@ for (const line of input.split(getLineSeparator())) {
 }
 console.log('Part 1 Horizontal:', totalXmases);
 
-// Diagonal
-let _totalXmasesPrev = totalXmases;
-for (let y = 0; y < grid.length; y++) {
-    const line = grid[y];
-
-    for (let x = 0; x < line.length; x++) {
-        const char = line[x];
-
-        if (char === 'X') {
-            const diagonalLeft = getDiagonal(grid, x, y, false)
-            const diagonalRight = getDiagonal(grid, x, y)
-
-            if (diagonalLeft.join('') === 'XMAS') totalXmases++;
-            if (diagonalRight.join('') === 'XMAS') totalXmases++;
-        } else if (char === 'S') {
-            const diagonalLeft = getDiagonal(grid, x, y, false)
-            const diagonalRight = getDiagonal(grid, x, y)
-
-            if (diagonalLeft.join('') === 'SAMX') totalXmases++;
-            if (diagonalRight.join('') === 'SAMX') totalXmases++;
-        }
-    }
-}
-console.log('Part 1 Diagonal:', totalXmases - _totalXmasesPrev);
-
 // Vertical
+let _totalXmasesPrev = totalXmases;
+const rotatedGrid = transpose(grid);
+for (const _line of rotatedGrid) {
+    const line = _line.join('');
+    console.log(line);
+    totalXmases += line.matchAll(/XMAS/g).toArray().length + line.matchAll(/SAMX/g).toArray().length;
+}
+
+console.log('Part 1 Vertical:', totalXmases - _totalXmasesPrev);
+
+// Diagonal
 _totalXmasesPrev = totalXmases;
 for (let y = 0; y < grid.length; y++) {
     const line = grid[y];
 
     for (let x = 0; x < line.length; x++) {
-        const char = line[x];
+        const strForwards = getDiagonal(grid, y, x);
+        if(strForwards === "XMAS" || strForwards === 'SAMX') totalXmases++;
 
-        if (char === 'X') {
-            const vertical = getVertical(grid, x, y);
-            if (vertical.join('') === 'XMAS') totalXmases++;
-        } else if (char === 'S') {
-            const vertical = getVertical(grid, x, y);
-            if (vertical.join('') === 'SAMX') totalXmases++;
-        }
+        const strBackwards = getDiagonal(grid, y, x, false);
+        if(strBackwards === "XMAS" || strBackwards === 'SAMX') totalXmases++;
     }
 }
-console.log('Part 1 Vertical:', totalXmases - _totalXmasesPrev);
+console.log('Part 1 Diagonal:', totalXmases - _totalXmasesPrev);
 
 console.log('Part 1 Total:', totalXmases);
 
+function getDiagonal(grid: string[][], y: number, x: number, forwards = true): string {
+    if(y+3 >= grid.length) return "";
+    if(forwards && x+3 > grid[0].length) return "";
+    if(!forwards && x-3 < 0) return "";
 
-function getDiagonal(input: string[][], startIndexX: number, startIndexY: number, rightwards = true): string[] {
-    if (startIndexY + 3 >= input.length) return [];
-
-    if (rightwards) {
-        if (startIndexX + 3 >= input[startIndexY].length) return [];
-
-        return [input[startIndexY][startIndexX], input[startIndexY + 1][startIndexX + 1], input[startIndexY + 2][startIndexX + 2], input[startIndexY + 3][startIndexX + 3]]
-    } else {
-        if (startIndexX - 3 <= input[startIndexY].length) return [];
-
-        return [input[startIndexY][startIndexX], input[startIndexY + 1][startIndexX - 1], input[startIndexY + 2][startIndexX + 2], input[startIndexY + 3][startIndexX - 3]]
-    }
-}
-
-function getVertical(input: string[][], startIndexX: number, startIndexY: number): string[] {
-    if (startIndexY + 3 >= input.length) return [];
-
-    return [input[startIndexY][startIndexX], input[startIndexY + 1][startIndexX], input[startIndexY + 2][startIndexX], input[startIndexY + 3][startIndexX]]
+    return (forwards ? [
+        grid[y][x],
+        grid[y + 1][x + 1],
+        grid[y + 2][x + 2],
+        grid[y + 3][x + 3]
+    ] : [
+        grid[y][x],
+        grid[y + 1][x - 1],
+        grid[y + 2][x - 2],
+        grid[y + 3][x - 3]
+    ]).join('');
 }
--- util.ts
+++ util.ts
@@ -1,6 +1,12 @@
 import * as path from "jsr:@std/path";
 import * as mod from "node:os";
 
+/**
+ * Get input as one continuous string
+ * @param importMeta import.meta
+ * @param sample use sample data (input_sample.txt)
+ * @returns input string
+ */
 export async function getInput(importMeta: ImportMeta, sample: boolean = false): Promise<string> {
     return await Deno.readTextFile(`${getModuleDir(importMeta)}/input${sample ? '_sample' : ''}.txt`);
 }
@@ -9,6 +15,15 @@ function getModuleDir(importMeta: ImportMeta): string {
     return path.resolve(path.dirname(path.fromFileUrl(importMeta.url)));
 }
 
-export function getLineSeparator(){
+export function getLineSeparator() {
     return mod.platform() === 'win32' ? '\r\n' : '\n';
+}
+
+/**
+ * Transposes (rotates 90 degrees) a two dimensional array
+ * @param arr 2D Array
+ * @returns 90 degree rotated 2D Array
+ */
+export function transpose(arr: string[][]) {
+    return Object.keys(arr[0]).map((_, i) => arr.map((r) => r[i]));
 }
